19187cb6bfd5e51982f0a07b5652c29989ad65c6
Dawid <davidblonsky88@gmail.com>
2019-06-06 17:04:38 +0200

    first commit

---
+++ tests.py
@@ -0,0 +1,37 @@
+from mine import calculate
+
+
+def test_data_test():
+
+    assert calculate(1, [("dolan.png", 126, 5), ("expanding_brain.jpeg", 421, 10)]) == (
+        15,
+        {"dolan.png", "expanding_brain.jpeg"},
+    )
+
+    assert calculate(
+        1,
+        [
+            ("rollsafe.jpg", 205, 6),
+            ("sad_pepe_compilation.gif", 410, 10),
+            ("yodeling_kid.avi", 605, 12),
+        ],
+    ) == (22, {"sad_pepe_compilation.gif", "yodeling_kid.avi"})
+
+
+def test_no_data():
+    assert calculate(0,[]) == (0,set())
+
+def test_duplicate_data():
+    assert calculate(
+        1,
+        [
+            ("rollsafe.jpg", 205, 6),
+            ("sad_pepe_compilation.gif", 410, 10),
+            ("yodeling_kid.avi", 605, 12),
+            ("rollsafe.jpg", 205, 6),
+            ("sad_pepe_compilation.gif", 410, 10),
+            ("yodeling_kid.avi", 605, 12),
+        ],
+    ) == (22, {"sad_pepe_compilation.gif", "yodeling_kid.avi"})
+
+
