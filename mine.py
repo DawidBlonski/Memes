71d9294811dd28532a01c119c88c858c5dde8fb5
Dawid <davidblonsky88@gmail.com>
2019-06-06 23:11:49 +0200

    update mine.py

--- mine.py
+++ mine.py
@@ -9,8 +9,6 @@ To get the most efficient code solution I adapt solution from  site
 https://codereview.stackexchange.com/questions/20569/dynamic-programming-knapsack-solution/20581#20581
 "
 
-
-
 """
 from typing import List, Tuple, Set
 from functools import lru_cache
@@ -27,28 +25,18 @@ def calculate(usb_size: int, memes: List[Tuple[str, int, int]]) -> Tuple[int, Se
     @lru_cache(maxsize=None)
     def bestvalue(i, usb_size):
         if usb_size < 0:
+            'Return infinity helps find the smallest value'
             return float('-inf')
         if i == 0:
             return 0
         _, weight, value = memes[i - 1]
-
         return max(
             bestvalue(i - 1, usb_size), bestvalue(i - 1, usb_size - weight) + value
         )
 
     for i in reversed(range(len(memes))):
         if bestvalue(i + 1, usb_size) != bestvalue(i, usb_size):
-            print(memes[i])
             result += memes[i][2]
             mems.add(memes[i][0])
             usb_size -= memes[i][1]
     return result, mems
-
-
-usb_size = 1
-memes = [
-('rollsafe.jpg', 205, 6),
-('sad_pepe_compilation.gif', 410, 10),
-('yodeling_kid.avi', 605, 12)
-]
-print(calculate(usb_size, memes))
